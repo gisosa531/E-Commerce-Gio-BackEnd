993aa203d87a8c93760f7e6ede3b51c8b93dcb5a
Giovanni Sosa <gio53196@gmail.com>
2021-09-12 08:36:37 -0700

    Added models columns with some validations and sequelized server.js file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+}),
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    foreignKey :'product_id',
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'products_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    foreignKey : 'tag_id',
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
