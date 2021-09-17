9c097f8e83010bb4f4c5e9a0433a360b2f160554
Giovanni Sosa <gio53196@gmail.com>
2021-09-16 17:43:06 -0700

    Adding CRUD data to category and product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,86 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+    // be sure to include its associated Products
+  const categoryInfo = await Category.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ],
+  })
+  .then((categories) => {
+    res.json(categories);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryInfo = await Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [ 
+      {
+      model: Product
+      }
+  ],
+})
+  .then((categories) => {
+    res.json(categories);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const categoryInfo = await Category.create({
+    category_data: req.body.category_data,
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    Category.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    })
+    .then((category) => res.status(200).json(category))
+  }
+    catch (err) { 
+      res.status(400).json(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const tag_id = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tag_id) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+   }
+    res.status(200).json(tag_id);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const productInfo = await Product.findAll({
+    include: [
+      { 
+        model: Category,
+      },
+      {
+        model: Tag ,
+        through: ProductTag,
+      },
+    ],
+  }).then((products) => res.json(products))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
